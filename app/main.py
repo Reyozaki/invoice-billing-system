1c219fc735ca26c9c1507223fe6804c0c494f24a
Ujan Dongol <dongolujan@gmail.com>
2025-06-27 19:50:28 +0545

    Added Invoices and endpoints

--- app/main.py
+++ app/main.py
@@ -3,13 +3,14 @@ import os
 sys.path.append(os.path.dirname(os.path.abspath(__file__)))
 
 from fastapi import FastAPI
-from .routers import customer, product, auth
+from .routers import customer, product, auth,invoices
 from app.database import engine, Base
 
 app= FastAPI()
 app.include_router(customer.router)
 app.include_router(product.router)
 app.include_router(auth.router)
+app.include_router(invoices.router)
 
 
 Base.metadata.create_all(bind=engine)
