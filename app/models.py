88ef8b936de4e49fb74ae5b357737f73becf601c
Ujan Dongol <dongolujan@gmail.com>
2025-06-29 13:25:12 +0545

    Added invoices and made changes at models and schemas.

--- app/models.py
+++ app/models.py
@@ -25,3 +25,13 @@ class Sale(Base):
     
     customer_id= Column(BigInteger, primary_key= True)
     product_id= Column(Integer, ForeignKey("products.product_id"), index= True, nullable= False)
+    
+class Invoices(Base):
+    __tablename__ = "invoices"
+
+    invoice_id = Column(BigInteger, primary_key=True, index=True)
+    customer_id = Column(BigInteger, ForeignKey("customers.customer_id"), nullable=False)
+    date = Column(DateTime(timezone=True), server_default=func.now())
+    total_amount = Column(Numeric(10, 2), nullable=False)
+    status = Column(String(20), default="unpaid")
+    
--- app/schemas.py
+++ app/schemas.py
@@ -2,6 +2,7 @@ from pydantic import BaseModel, EmailStr, StringConstraints    # type: ignore
 from typing import Optional, Literal, List
 from decimal import Decimal
 from typing import Annotated
+from datetime import datetime
 
 class StoreBase(BaseModel):    
     store_name: str
@@ -30,3 +31,15 @@ class UpdateCustomer(CustomerBase):
     phone: Optional[Annotated[str, StringConstraints(pattern= r"^\d{10}$")]]= None
     address: Optional[str]
     tax_id: Optional[int]
+
+
+class InvoiceBase(BaseModel):
+    invoice_id: Optional[int] = None
+    customer_id: int
+    total_amount: Decimal
+    status: Optional[str] = "unpaid"
+    date: Optional[datetime] = None
+
+    class Config:
+        orm_mode = True
+    
